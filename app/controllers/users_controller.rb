565cd3ddb02bb23c7127c4f26c948d38cb24a9e4
Shintaro-s-b <shintaro_s_b@yahoo.co.jp>
2017-07-31 15:52:04 +0000

    [add] tutorial complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    if params[:username] == 'ryooopan'
+      @user = User.find_by(:username => 'ryooopan')
+    elsif params[:username] == 'moyahima'
+      @user = User.find_by(:username => 'moyahima')
+    end
   end
+
 end
