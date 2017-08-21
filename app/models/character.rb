0ef97068dafe7c2e7750c0e326e7a9569cd30fa1
shintaro <shintaro_s_b@yahoo.co.jp>
2017-08-22 00:37:02 +0900

    [add] モデルのテストもうちょっと追加

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ApplicationRecord
   def self.getMiho
     return Character.find(1)
   end
+  def self.getBySchoolId(school_id)
+    return Character.where(school_id: school_id)
+  end
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Character, type: :model do
       expect( Character.getMiho['chara_name'] ).to eq 'miho'
     end
   end
+  describe 'find by school_id' do
+    it 'ooarai' do
+      expect( Character.getBySchoolId(1).count ).to eq 32
+    end
+    it 'keizoku' do
+      expect( Character.getBySchoolId(8).count ).to eq 3
+    end
+    it 'st_groriana' do
+      expect( Character.getBySchoolId(3).count ).to eq 5
+    end
+  end
 end
